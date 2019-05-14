912a140b4032b787fd860d38c770597477805772
Berat9629 <berat29_29@hotmail.com>
2019-05-14 16:40:20 +0200

    loesung

--- src/test/java/ohm/softa/a06/tests/ICNDBTests.java
+++ src/test/java/ohm/softa/a06/tests/ICNDBTests.java
@@ -1,9 +1,15 @@
 package ohm.softa.a06.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ohm.softa.a06.ICNDBApi;
+import ohm.softa.a06.JokeAdapter;
 import ohm.softa.a06.model.Joke;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import retrofit2.Retrofit;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -20,6 +26,12 @@ class ICNDBTests {
 	private static final Logger logger = LogManager.getLogger(ICNDBTests.class);
 	private static final int REQUEST_COUNT = 10;
 
+	private ICNDBApi icndbApi;
+
+	@BeforeEach
+	void setup(){
+		Gson gson = new GsonBuilder().registerTypeAdapter(Joke.class, new JokeAdapter()).create();
+	}
 	@Test
 	void testCollision() throws IOException {
 		Set<Integer> jokeNumbers = new HashSet<>();
